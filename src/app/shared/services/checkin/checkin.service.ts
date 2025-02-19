f252e462804d11ed7483c7cd4cf601be022ea47e
anukriti6 <anukritigupta1988@gmail.com>
2025-02-19 13:35:22 +0530

    added apollo client and tested mutation working fine

--- src/app/shared/services/checkin/checkin.service.ts
+++ src/app/shared/services/checkin/checkin.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CheckinService {
-
-  constructor() { }
-  doCheckin(){
+  constructor(private readonly apollo: Apollo) {}
+  doCheckin() {
     // call api for checkin
     console.log('checkin api called.');
+    this.apollo
+      .mutate({
+        mutation: gql`
+          mutation XYZ {
+            doCheckin(bookingCode: "K12345", fname: "GUPTA") {
+              message
+            }
+          }
+        `,
+      })
+      .subscribe((result: any) => {
+        // this.rates = result.data?.rates;
+        // this.loading = result.loading;
+        // this.error = result.error;
+        console.log(result.data);
+      });
   }
 }
